7ac0b4856d953110e59bd15c06a902d7190f5b4d
claudio-navarro-martinez <claudio.oami@gmail.com>
2022-09-08 13:26:14 +0100

    poco a poco

--- commands/topomlcommands.go
+++ commands/topomlcommands.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"fmt"
 	"log"
 	"time"
 
@@ -11,6 +12,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var pipelineid string
+
 var rootCmd = &cobra.Command{
 	Use:   "topoml",
 	Short: "topoml",
@@ -18,9 +21,9 @@ var rootCmd = &cobra.Command{
 }
 
 var listFileAzure = &cobra.Command{
-	Use: "list",
-	Short: "listtopoml",
-	Long: `list TOPO Machine Learning`,
+	Use: "azure",
+	Short: "azure subcommand",
+	Long: `azure TOPO Machine Learning`,
 	Run: func(cmd *cobra.Command, args []string) {
 			if len(args) > 1 {
 				log.Fatal("subcommand list only take one argument")
@@ -29,37 +32,68 @@ var listFileAzure = &cobra.Command{
 	},
 }
 
-var createcmd = &cobra.Command{
+var pipelinecmd = &cobra.Command{
+	Use: "pipeline",
+	Short: "CRUD pipelines, ml algo and other things to come",
+	Long: `create pipelines, ml algo and other things to come`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+var pipelinecreatecmd = &cobra.Command{
 	Use: "create",
 	Short: "create pipelines, ml algo and other things to come",
 	Long: `create pipelines, ml algo and other things to come`,
 	Run: func(cmd *cobra.Command, args []string) {
-	},
-}
-
-var pipelinecmd = &cobra.Command{
-	Use: "pipeline",
-	Short: "create pipelines, ml algo and other things to come",
-	Long: `create pipelines, ml algo and other things to come`,
-	Run: func(cmd *cobra.Command, args []string) {
-		
 		conn := pgdb.Connect()
 
 		r1 := &pgdb.Registry{
-			RegistryId: "dockerhub5",
+			RegistryId: "dockerhub6",
 		}
-		i1 := &pgdb.Image{ImageId: "image5"}
+		i1 := &pgdb.Image{ImageId: "image6"}
 		p1 := &pgdb.Pipeline {
 			SecretName: "no te lo digo",
 			Crontab: time.Now(),
-			PipelineId : "pipe 5",
+			PipelineId : "pipe 6",
 			Output: "fichero.salida",
 			Cloudname: "azure",
 			RegistryId: r1.RegistryId,
 			ImageId: i1.ImageId,
 		}
 		SavePipeline(conn,p1,r1,i1)
+
+	},
+}
+
+var pipelinelistcmd = &cobra.Command{
+	Use: "list",
+	Short: "list pipelines, ml algo and other things to come",
+	Long: `list pipelines, ml algo and other things to come`,
+	Run: func(cmd *cobra.Command, args []string) {
+		db := pgdb.Connect()
+		var pipelines []pgdb.Pipeline
+		result := db.Find(&pipelines)
+		if result.RowsAffected > 0 {
+			for _, v := range pipelines {
+				fmt.Println(v.PipelineId)
+			}
+		}
+	},
+}
+
+var pipelinedeletecmd = &cobra.Command{
+	Use: "delete",
+	Short: "delete pipelines, ml algo and other things to come",
+	Long: `list pipelines, ml algo and other things to come`,
+	Run: func(cmd *cobra.Command, args []string) {
+		db := pgdb.Connect()
 		
+		result := db.Delete(&pgdb.Pipeline{PipelineId: pipelineid})
+		if result.RowsAffected > 0 {
+				fmt.Println("pipeline borrada")
+			
+		}
 	},
 }
 
@@ -69,14 +103,18 @@ func SavePipeline(db *gorm.DB, p *pgdb.Pipeline, r *pgdb.Registry, i *pgdb.Image
 	// db.AutoMigrate(r)
 	// db.AutoMigrate(i)
 	p.Insert(db)
-	r.Insert(db)
-	i.Insert(db)
+	//r.Insert(db)
+	//i.Insert(db)
 }
 
 func init() {
+	
 	rootCmd.AddCommand(listFileAzure)
-	rootCmd.AddCommand(createcmd)
-	createcmd.AddCommand(pipelinecmd)
+	rootCmd.AddCommand(pipelinecmd)
+	pipelinecmd.AddCommand(pipelinecreatecmd)
+	pipelinecmd.AddCommand(pipelinelistcmd)
+	pipelinecmd.AddCommand(pipelinedeletecmd)
+	pipelinedeletecmd.Flags().StringVarP(&pipelineid, "pipeid", "p", "", "Pipeline Id to delete")
 }
 
 func Execute() {
